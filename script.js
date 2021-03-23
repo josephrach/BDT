3e5326417c71f7cfba1160776f3d221ba103fd62
Sir Joseph <sirjosephholmes@gmail.com>
2021-03-23 12:09:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// mengambil elemen tombol
+// var tombolBeranda = document.getElementsByClassName('menu_beranda');
+
+// mengambil halamannya
+// var isi {
+//   halamanBeranda : document.getElementById('bagian_konten');
+//   halamanCustom : document.getElementById('bagian_konten2');
+// }
+
+
+// membuat fungsi geser halamannya
+// function geserHalamanBeranda() {
+//   this.halamanBeranda = isi.halamanBeranda;
+//   this.halamanCustom = isi.halamanCustom;
+//   halamanBeranda.style.left = 0;
+//   halamanCustom.style.left = -1500;
+//   return halamanBeranda,halamanCustom;
+// }
+
+
+// menambahkan event ke dalam tombol
+// tombolBeranda.onClick = geserHalamanBeranda();
+
+
